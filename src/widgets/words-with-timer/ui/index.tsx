1c2168722d50f98dbfae6d22f66c4877dfcdc10d
BonMarty <BonMarty0207@gmail.com>
2024-08-12 11:41:05 +0300

    start of setup for mobile device

--- src/widgets/words-with-timer/ui/index.tsx
+++ src/widgets/words-with-timer/ui/index.tsx
@@ -1,9 +1,18 @@
 import { Timer, Words } from '@/entities';
 import { ChangeTimeLimit, UserTypings } from '@/features';
 import { useAppSelector } from '@/shared';
+import React from 'react';
 
 export const WordsWithTimer = () => {
   const timeLimit = useAppSelector((state) => state.timer.timeLimit);
+
+  const inputRef = React.useRef<HTMLInputElement>(null);
+
+  const onClick = () => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  };
 
   return (
     <>
@@ -11,9 +20,10 @@ export const WordsWithTimer = () => {
       <div className="flex-1 flex items-center w-full">
         <div className="relative flex flex-col gap-10 w-full">
           <Timer value={timeLimit} />
-          <div className="relative font-mono h-32 overflow-hidden">
+          <div className="relative font-mono h-32 overflow-hidden" onClick={onClick}>
             <Words />
             <UserTypings />
+            <input className="hidden" ref={inputRef} />
           </div>
         </div>
       </div>
