3c4f93109ba2e46636e01c18e2ca5050eb1ccad6
BonMarty <BonMarty0207@gmail.com>
2024-08-12 12:08:19 +0300

    add start to onchange in input for mobile devices

--- src/widgets/words-with-timer/ui/input-for-mobile-devices/index.tsx
+++ src/widgets/words-with-timer/ui/input-for-mobile-devices/index.tsx
@@ -1,9 +1,19 @@
-import { setUserInput } from '@/entities';
+import { setIsStarted, setUserInput } from '@/entities';
 import { useAppDispatch, useAppSelector } from '@/shared';
 
 export const InputForMobileDevices = () => {
   const userInput = useAppSelector((state) => state.words.userInput);
   const dispatch = useAppDispatch();
 
-  return <input value={userInput} onChange={(e) => dispatch(setUserInput(e.target.value))} className="w-full h-full z-10 opacity-0 lowercase" />;
+  return (
+    <input
+      autoCapitalize="none"
+      value={userInput}
+      onChange={(e) => {
+        dispatch(setIsStarted(true));
+        dispatch(setUserInput(e.target.value));
+      }}
+      className="w-full h-full z-10 opacity-0 lowercase"
+    />
+  );
 };
